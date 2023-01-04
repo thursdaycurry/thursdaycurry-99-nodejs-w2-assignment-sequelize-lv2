1662a1350727ddd150b1622eeae824c329ed45dd
thursdaycurry <thursdaycurry@gmail.com>
2023-01-04 15:25:36 +0900

    add Likes migration and model

--- models/users.js
+++ models/users.js
@@ -30,6 +30,13 @@ module.exports = (sequelize, DataTypes) => {
         sourceKey: "userId", // 3. Users 모델의 userId 컬럼을
         foreignKey: "UserId", // 4. Comments 모델의 UserId 컬럼과 연결합니다.
       });
+
+      // 1. Users 모델에서
+      this.hasMany(models.Likes, {
+        // 2. Likes 모델에게 1:N 관계 설정을 합니다.
+        sourceKey: "userId", // 3. Users 모델의 userId 컬럼을
+        foreignKey: "UserId", // 4. Likes 모델의 UserId 컬럼과 연결합니다.
+      });
     }
   }
 
