b08fa2da631b3b90049ea8ecf093316ef7f7de78
thursdaycurry <thursdaycurry@gmail.com>
2023-01-04 15:25:56 +0900

    migration and models for Likes

---
+++ migrations/20230104060703-create-likes.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Likes", {
+      likeId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UserId: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users", // Users 모델을 참조합니다.
+          key: "userId", // Users 모델의 userId를 참조합니다.
+        },
+        onDelete: "CASCADE", // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
+      },
+      PostId: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Posts", // Users 모델을 참조합니다.
+          key: "postId", // Users 모델의 userId를 참조합니다.
+        },
+        onDelete: "CASCADE", // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Likes");
+  },
+};
